bf522e351982496cd6c1122d066466775f1774f0
kumartech-maker <erpvanes@gmail.com>
2025-07-22 09:55:17 +0530

    Update project.py

--- app/routes/project.py
+++ app/routes/project.py
@@ -1,15 +1,21 @@
-# routes/project.py
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 from werkzeug.utils import secure_filename
 from datetime import datetime
 import os
+
 from ..models import db, Project, Vendor
 
 project_bp = Blueprint('project', __name__)
 
+# -------------------------------
+# Route: Create New Project
+# -------------------------------
 @project_bp.route('/new_project', methods=['GET', 'POST'])
 def new_project():
     vendors = Vendor.query.all()
+
+    # Debug print to confirm vendors
+    print("Loaded vendors:", [(v.id, v.name) for v in vendors])
 
     if request.method == 'POST':
         enquiry_id = generate_enquiry_id()
@@ -29,6 +35,7 @@ def new_project():
         filename = None
         if diagram_file:
             filename = secure_filename(diagram_file.filename)
+            os.makedirs('uploads', exist_ok=True)
             diagram_file.save(os.path.join('uploads/', filename))
 
         project = Project(
@@ -55,6 +62,22 @@ def new_project():
     enquiry_id = generate_enquiry_id()
     return render_template('new_project.html', vendors=vendors, enquiry_id=enquiry_id, projects=projects)
 
+# -------------------------------
+# Route: Seed Dummy Vendors (TEMP)
+# -------------------------------
+@project_bp.route('/seed_vendors')
+def seed_vendors():
+    if not Vendor.query.first():
+        v1 = Vendor(name="ABC Enterprises", gst="29ABCDE1234F2Z5", address="Chennai, TN")
+        v2 = Vendor(name="XYZ Solutions", gst="33XYZDE5678G1Z9", address="Coimbatore, TN")
+        db.session.add_all([v1, v2])
+        db.session.commit()
+        return "Dummy vendors added"
+    return "Vendors already exist"
+
+# -------------------------------
+# Utility: Generate Enquiry ID
+# -------------------------------
 def generate_enquiry_id():
     last = Project.query.order_by(Project.id.desc()).first()
     next_num = 1 if not last else last.id + 1
